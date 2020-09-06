5ed6828c5453415f3b25281563235d11e6436b16
Halú de Cássia <halu.amancio@imusics.com>
2020-09-06 01:30:27 -0300

    Componentiza Main

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Form from "../Form";
+
+const Main = () => {
+  return (
+    <main className="w-full flex justify-center -mt-64">
+      <Form />
+    </main>
+  );
+};
+
+export default Main;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 
 import Header from "../components/Header";
-import Form from "../components/Form";
-
-import Date from "../assets/svg/Icon/Date.svg";
-import Image from "../assets/svg/Icon/Image.svg";
-import Logo from "../assets/svg/Icon/Logo.svg";
-import Menu from "../assets/svg/Icon/Menu.svg";
-import Person from "../assets/svg/Icon/Person.svg";
-import Place from "../assets/svg/Icon/Place.svg";
-import Search from "../assets/svg/Icon/Search.svg";
+import Main from "../components/Main";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <main className="w-full flex justify-center -mt-64">
-        <Form />
-      </main>
+      <Main />
     </div>
   );
 };
